fa629af11ae4013b9ba17cbf2999af126abfbfa8
Primo <zhukejin@msn.com>
2016-06-16 13:27:26 +0800

    button 组件现在支持 style-btn 样式传递

--- Js/button.js
+++ Js/button.js
@@ -1,12 +1,14 @@
 Vue.component('vButton', {
-    template: "<span v-el:father style='display: inline-block;'><template v-if='material==\"span\"'>" +
-         "<span :style=\"{width: width + 'px', height: height + 'px', marginLeft: left + 'px', marginRight: right + 'px', lineHeight: '24px'}\" class='zq-btn' :class=\"btnClass\"><slot></slot></span>" + 
+    template: "<span v-el:father style='display: inline-block;' :style='style'><template v-if='material==\"span\"'>" +
+    "<span class='zq-btn' :style=\"styleBtn\" :class=\"btnClass\"><slot></slot></span>" +
     "</template>" +
     "<template v-else>"+
-    "<button :type='htmlType' :disabled='disabled' :style=\"{width: width + 'px', height: height + 'px', marginLeft: left + 'px', marginRight: right + 'px'}\" class='zq-btn' :class=\"btnClass\"><slot></slot></button>"
+    "<button :type='htmlType' :disabled='disabled1' :style=\"styleBtn\" class='zq-btn' :class=\"btnClass\"><slot></slot></button>"
     +"</template></span>",
 
     props: {
+        style: Object,
+        styleBtn: Object,
         type: String,
         htmlType: String,
         disabled: Boolean,
@@ -19,19 +21,32 @@ Vue.component('vButton', {
     data: function () {
         if (this.disabled === undefined) this.disabled = true;
         return {
-            disabled : this.disabled,
-            htmlType : this.htmlType || 'button'
+            disabled1 : this.disabled,
         }
     },
     computed: {
         btnClass: function () {
             var cls = ['zq-' + this.type + '-btn']
-            if (this.disabled) { 
+            if (this.disabled) {
                 cls.push('zq-disable-btn');
-                this.$off(['click'])
             };
             if (!this.type || this.type == 'ghost') cls.push('zq-default-btn');
             return cls.join(" ");
         }
+    },
+    beforeCompile: function () {
+        var style = {
+            width: this.width + 'px',
+            height: this.height + 'px',
+            marginLeft: this.left + 'px',
+            marginRight: this.right + 'px'
+        };
+
+        if (this.material == 'span') {
+            style.lineHeight = this.height ? (this.height - 10 + 'px') : '24px'
+        }
+
+        this.styleBtn = $$.assign(style, this.styleBtn);
+
     }
 });
